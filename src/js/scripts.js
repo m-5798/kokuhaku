b3c1d5e4c230dc83b176a8c02866abc15a38205f
Masashi.Sato <masashi.sato@brainpad.co.jp>
2018-12-02 01:07:25 +0900

    修正

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,10 +5,34 @@ if (typeof Site === 'undefined') {
 
     var d = W.document;
 
+    S.getDevice = function() {
+      var ua = navigator.userAgent
+        , res = '';
+      if(ua.indexOf('iPhone') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0) {
+        res = 'sp';
+      } else if(ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0) {
+        res = 'tablet';
+      } else {
+        res = 'other';
+      }
+      return res;
+    };
+
+    S.anchorScroll = function() {
+      $('a[href^="#"]').click(function() {
+        var speed = 400
+          , href= $(this).attr('href')
+          , target = $(href == '#' || href == '' ? 'html' : href)
+          , position = target.offset().top;
+        position = position - 70;
+        $('body, html').animate({scrollTop:position}, speed, 'swing');
+        return false;
+      });
+    };
 
   })(Site, window);
 }
 
 $(document).ready(function () {
-
+  Site.anchorScroll();
 });
