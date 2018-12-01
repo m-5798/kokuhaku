34206e1160427adeb722ee5ebef0007c7a82714c
Masashi.Sato <masashi.sato@brainpad.co.jp>
2018-12-01 15:16:45 +0900

    first commit

---
+++ src/js/scripts.js
@@ -0,0 +1,14 @@
+if (typeof Site === 'undefined') {
+  var Site = {};
+  (function(S, W) {
+    'use strict';
+
+    var d = W.document;
+
+
+  })(Site, window);
+}
+
+$(document).ready(function () {
+
+});
