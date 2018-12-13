5677c5b7661bf1fac5f3ea3eabeaf4a1044b70d5
Masashi.Sato <masashi.sato@brainpad.co.jp>
2018-12-14 02:52:31 +0900

    修正

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,7 +7,8 @@ if (typeof Site === 'undefined') {
 
     S.getDevice = function() {
       var ua = navigator.userAgent
-        , res = '';
+        , res = ''
+        ;
       if(ua.indexOf('iPhone') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0) {
         res = 'sp';
       } else if(ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0) {
@@ -23,10 +24,72 @@ if (typeof Site === 'undefined') {
         var speed = 400
           , href= $(this).attr('href')
           , target = $(href == '#' || href == '' ? 'html' : href)
-          , position = target.offset().top;
-        position = position - 70;
+          , position = target.offset().top
+          ;
+        console.log(href);
+        position = (href === '#lede') ? position : position - 70;
         $('body, html').animate({scrollTop:position}, speed, 'swing');
         return false;
+      });
+    };
+
+    S.toggleInfo = function() {
+      var $info = $('.p-information')
+        , $toggle = $('[data-kkhk-toggle]')
+        , cls = 'p-information-open'
+        ;
+      $toggle.on('click', function() {
+        $info.toggleClass(cls);
+      });
+    };
+
+    S.scrollOut = function() {
+      var logo = '[data-kkhk-logo]'
+        , lede1 = '[data-kkhk-lede1]'
+        , lede2 = '[data-kkhk-lede2]'
+        , lede3 = '[data-kkhk-lede3]'
+        , businessH1 = '[data-kkhk-business-h1]'
+        , businessH2_1 = '[data-kkhk-business-h2_1]'
+        , businessH2_2 = '[data-kkhk-business-h2_2]'
+        , worksH1 = '[data-kkhk-works-h1]'
+        , memberH1 = '[data-kkhk-member-h1]'
+        , memberH2_1 = '[data-kkhk-member-h2_1]'
+        , memberH2_2 = '[data-kkhk-member-h2_2]'
+        , memberH2_3 = '[data-kkhk-member-h2_3]'
+        , memberH2_4 = '[data-kkhk-member-h2_4]'
+        , variation = '-show'
+        ;
+
+      $.each([
+        lede1,
+        lede3,
+        businessH1,
+        businessH2_1,
+        businessH2_2,
+        worksH1,
+        memberH1,
+        memberH2_1,
+        memberH2_2,
+        memberH2_3,
+        memberH2_4,
+      ], function(i, obj) {
+        var showClass = $(obj).attr('class') + variation;
+        ScrollOut({
+          targets: obj,
+          once: true,
+          onShown: function() {
+            $(obj).addClass(showClass);
+          }
+        });
+      });
+
+      ScrollOut({
+        targets: lede2,
+        once: true,
+        onShown: function(el) {
+          $(logo).addClass($(logo).attr('class') + variation);
+          $(el).addClass($(el).attr('class') + variation);
+        }
       });
     };
 
@@ -35,60 +98,6 @@ if (typeof Site === 'undefined') {
 
 $(document).ready(function () {
   Site.anchorScroll();
-
-  var $info = $('.p-information')
-    , $toggle = $('[data-kkhk-toggle]')
-    , cls = 'p-information-open'
-    ;
-  $toggle.on('click', function() {
-    $info.toggleClass(cls);
-  });
-
-  var logo = '[data-kkhk-logo]'
-    , lede1 = '[data-kkhk-lede1]'
-    , lede2 = '[data-kkhk-lede2]'
-    , lede3 = '[data-kkhk-lede3]'
-    , businessH1 = '[data-kkhk-business-h1]'
-    , businessH2_1 = '[data-kkhk-business-h2_1]'
-    , businessH2_2 = '[data-kkhk-business-h2_2]'
-    , worksH1 = '[data-kkhk-works-h1]'
-    , memberH1 = '[data-kkhk-member-h1]'
-    , memberH2_1 = '[data-kkhk-member-h2_1]'
-    , memberH2_2 = '[data-kkhk-member-h2_2]'
-    , memberH2_3 = '[data-kkhk-member-h2_3]'
-    , memberH2_4 = '[data-kkhk-member-h2_4]'
-    , variation = '-show'
-    ;
-
-  $.each([
-    lede1,
-    lede3,
-    businessH1,
-    businessH2_1,
-    businessH2_2,
-    worksH1,
-    memberH1,
-    memberH2_1,
-    memberH2_2,
-    memberH2_3,
-    memberH2_4,
-  ], function(i, obj) {
-    var showClass = $(obj).attr('class') + variation;
-    ScrollOut({
-      targets: obj,
-      once: true,
-      onShown: function() {
-        $(obj).addClass(showClass);
-      }
-    });
-  });
-
-  ScrollOut({
-    targets: lede2,
-    once: true,
-    onShown: function(el) {
-      $(logo).addClass($(logo).attr('class') + variation);
-      $(el).addClass($(el).attr('class') + variation);
-    }
-  });
+  Site.toggleInfo();
+  Site.scrollOut();
 });
